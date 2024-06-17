87931fb297861a678419eac28ce8102c927b91f6
Elora Delpierre <edelpierre@exomind.fr>
2024-06-17 11:11:20 +0200

    Initial Commit

---
+++ Resilience/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Resilience
+//
+//  Created by Elora on 17/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Resilience/ResilienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResilienceApp.swift
+//  Resilience
+//
+//  Created by Elora on 17/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ResilienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
